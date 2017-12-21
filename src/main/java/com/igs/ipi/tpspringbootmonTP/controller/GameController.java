d6b8ede58055faee36ec4c431c8557faa38c840c
j6455lienv <venet.julien@gmail.com>
2017-12-21 21:26:43 +0100

    wip

--- src/main/java/com/igs/ipi/tpspringbootmonTP/controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringbootmonTP/controller/GameController.java
@@ -6,6 +6,7 @@ import com.igs.ipi.tpspringbootmonTP.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,15 +20,14 @@ public class GameController {
     private PartieEnCours partie;
 
     @GetMapping("/new")
-    public ModelAndView newGame(){
+    public ModelAndView newGame() {
         ModelAndView mav = new ModelAndView("game")
-                .addObject("title","Nouveau Puissance4")
+                .addObject("title", "Nouveau Puissance4")
                 .addObject("body", "test")
-                .addObject("route","/game/new")
+                .addObject("route", "/game/new")
                 .addObject("j1", gameService.newGame().getNom1())
                 .addObject("j2", gameService.newGame().getNom2())
-                .addObject("tab", gameService.newGame().getTab())
-                ;
+                .addObject("tab", gameService.newGame().getTab());
         return mav;
     }
 
@@ -53,5 +53,111 @@ public class GameController {
                 .addObject("showedName", showedName);
         return modelAndView;
     }
+
+    @GetMapping("/game/drop/{id}")
+    public String updateGame(@PathVariable Long id) {
+
+        int idz = id.intValue() - 1;
+        int[] moves = new int[2];
+
+
+        GameModel game = partie.getGm();
+
+        int playerId = game.getPlayerTurn();
+
+        int[][] gameTable = game.getGameTable();
+        for (int i = 0; i < 6; i++) {
+            if (gameTable[i][idz] == 1 || gameTable[i][idz] == 2) {
+                gameTable[i - 1][idz] = playerId;
+                break;
+            }
+        }
+        if (gameTable[5][idz] == 0) {
+            gameTable[5][idz] = playerId;
+        }
+
+        if (game.getPlayerTurn() == 1) {
+            game.setPlayerTurn(2);
+        } else game.setPlayerTurn(1);
+
+        //diagonals
+        for (int x = 3; x < 6; x++) {
+            for (int y = 3; y < 7; y++) {
+                if (gameTable[x][y] != 0) {
+                    if (gameTable[x - 1][y - 1] == gameTable[x][y]
+                            && gameTable[x - 2][y - 2] == gameTable[x][y]
+                            && gameTable[x - 3][y - 3] == gameTable[x][y]) {
+                        game.setWinner(gameTable[x][y]);
+                    }
+                }
+            }
+        }
+        for (int x = 3; x < 6; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (gameTable[x][y] != 0) {
+                    if (gameTable[x - 1][y + 1] == gameTable[x][y]
+                            && gameTable[x - 2][y + 2] == gameTable[x][y]
+                            && gameTable[x - 3][y + 3] == gameTable[x][y]) {
+                        game.setWinner(gameTable[x][y]);
+                    }
+                }
+            }
+        }
+
+        //horizontal
+        for (int x = 3; x < 6; x++) {
+            for (int y = 0; y < 7; y++) {
+                if (gameTable[x][y] != 0) {
+                    if (gameTable[x - 1][y] == gameTable[x][y]
+                            && gameTable[x - 2][y] == gameTable[x][y]
+                            && gameTable[x - 3][y] == gameTable[x][y]) {
+                        game.setWinner(gameTable[x][y]);
+                    }
+                }
+            }
+        }
+        //vertical
+        for (int x = 0; x < 6; x++) {
+            for (int y = 3; y < 7; y++) {
+                if (gameTable[x][y] != 0) {
+                    if (gameTable[x][y - 1] == gameTable[x][y]
+                            && gameTable[x][y - 2] == gameTable[x][y]
+                            && gameTable[x][y - 3] == gameTable[x][y]) {
+                        game.setWinner(gameTable[x][y]);
+                    }
+                }
+            }
+        }
+
+
+        game.setGameTable(gameTable);
+        partie.setGm(game);
+
+        if (game.getWinner() != 0) {
+            return "redirect:/game/won";
+        } else return "redirect:/game";
+
+    }
+
+    @GetMapping("/game/won")
+    public ModelAndView gameWon() {
+        String winner = new String();
+        GameModel game = partie.getGm();
+
+        if (game.getWinner() == 1) {
+            winner = game.getNom1();
+        } else if (game.getWinner() == 2) {
+            winner = game.getNom2();
+        }
+
+
+        ModelAndView modelAndView = new ModelAndView("won");
+        modelAndView
+                .addObject("winner", winner)
+                .addObject("game", game);
+
+        return modelAndView;
+    }
+
 
 }
